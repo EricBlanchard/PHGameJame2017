d72c4742280fd388d49c614a9f44869620b832a9
Kamikaze542 <gymnadude12@hotmail.com>
2017-11-25 00:18:33 -0500

    Added level progression structure, wave scaling, endwave and endgame callbacks

---
+++ Assets/Scripts/LevelProgression.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelProgression : MonoBehaviour {
+
+	private int currentWave = 0;
+	private float currentWaveDifficulty = 0;
+	private const int MAX_WAVES = 10;
+	private const float WAVE_SCALER = 2.7f;
+	private void LastEnemyDied()
+	{
+		OnWaveComplete();
+	}
+	//grantsClass.LastEnemyDied += LastEnemyDied();
+
+	private Action<int> m_OnWaveCompleted;
+
+	public Action<int> OnWaveCompleted
+	{
+		set { m_OnWaveCompleted += value; }
+	}
+
+	private void OnWaveComplete()
+	{
+		if (m_OnWaveCompleted != null)
+		{
+			m_OnWaveCompleted(currentWave);
+		}
+		currentWave++;
+
+		if (currentWave == 10)
+		{
+			EndTheGame();
+		}
+		DoScaling();
+	}
+
+	private Action m_EndGame;
+
+	public Action EndGame
+	{
+		set { m_EndGame += value; }
+	}
+
+	private void EndTheGame()
+	{
+		//Do EndGamy things
+		if (m_EndGame != null)
+		{
+			m_EndGame();
+		}
+	}
+
+	private Action<float> m_ScaleNextWave;
+
+	public Action<float> ScaleNextWave
+	{
+		set { m_ScaleNextWave += value; }
+	}
+
+	private void DoScaling()
+	{
+		currentWaveDifficulty = Mathf.Pow(currentWave / MAX_WAVES, WAVE_SCALER);
+		if (m_ScaleNextWave != null)
+		{
+			m_ScaleNextWave(currentWaveDifficulty);
+		}
+	}
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
