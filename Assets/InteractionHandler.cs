0a70761911b49f4e4efd88a1919ab183a99e801c
Wahid Shafique <wahidshafique@hotmail.com>
2017-11-27 23:41:15 -0500

    pushed old changes

---
+++ Assets/InteractionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour {
+    private Button pauseButton;
+	// Use this for initialization
+	void Start () {
+        pauseButton = GameObject.Find("PauseButton").GetComponent<Button>();
+        print(pauseButton);
+        pauseButton.onClick.AddListener(pauseGame);
+	}
+
+    void pauseGame() {
+        print("g");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
